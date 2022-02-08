fc6a0c50e41a3219b423f85fe3d0ed6ed7605667
rayanderson costa <ray.costa@outlook.com>
2022-02-08 19:21:23 -0300

    adicionei o index.js

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,21 @@
+const novoNome = "rayanderson"
+const idade = 27
+
+console.log(typeof novoNome)
+console.log(typeof idade)
+
+console.log(novoNome, idade)
+
+const nome = prompt("qual é seu nome?")
+const suaIdade = prompt("qual sua idade?")
+const endereco = prompt("onde voce mora")/** */
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+console.log(a, b)
+
+
+// 
