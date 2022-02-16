d9135a6196edf24a86b788c3227da5735b49c962
rayanderson costa <ray.costa@outlook.com>
2022-02-15 21:22:03 -0300

    stirngs-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,66 @@
+/**let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
+
+/**const Nome = prompt("digite seu nome ")
+const email = prompt("digite seu e-mail")
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o) ${Nome}`)*/
+
+
+/**const comidasPreferidas = ["farva", "açai", "lasanha", "cuscuz", "macarronada"];
+const comidas = comidasPreferidas.entries();
+
+
+console.log("Essas são as minhas comidas preferidas", comidasPreferidas)
+
+const comidaFavorita = prompt("sua comida favorita")
+comidasPreferidas[1] = comidaFavorita
+
+
+console.log(comidasPreferidas)*/
+
+
+/**let listaDeTarefas = []
+
+const tarefa1 = prompt("primeira tarefa do seu dia")
+const tarefa2 = prompt("segunda tarefa do seu dia")
+const tarefa3 = prompt("terceira tarefa do seu dia")
+
+console.log(listaDeTarefas)
+
+const tarefaRealizada = ("") */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
