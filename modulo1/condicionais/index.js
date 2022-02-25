8aa77d477f7d4f9835886276ad35d2a12a9b4d93
rayanderson costa <ray.costa@outlook.com>
2022-02-24 21:47:40 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,138 @@
+// ------------- Exercícios de interpretação de código -----------//
+
+/*const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if (numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}**/
+
+
+// ------------- Exercícios de interpretação de código -----------//
+
+/*let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)**/
+
+
+// ------------- Exercícios de interpretação de código -----------//
+
+/*const numero = Number(prompt("Digite o primeiro número.")) 
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	
+} else {
+    console.log("Essa mensagem é secreta!!!")
+}**/
+
+// -------- Exercícios de escrita de código --------//
+
+/*const idadeDoUsuario = Number(prompt("digite sua idade"))
+const menorIdade = 18
+
+
+    if( idadeDoUsuario > menorIdade){
+        console.log( "Você pode dirigir")
+    }else {
+        console.log("Você não pode dirigir")
+    }**/
+
+
+
+
+
+
+    // -------- Exercícios de escrita de código --------//
+
+
+/*const verificaTurno = prompt("Qual Turno Você estuda? Use 'M' para Matutino, 'V' para Vespertino ou 'N' para Noturno")
+
+
+
+    if (verificaTurno === "m"){
+        console.log("Bom Dia!")
+    } 
+    
+     if (verificaTurno === "v" ){
+        console.log("Boa Tarde!")
+    } 
+    
+    else if (verificaTurno === "n"){
+        console.log("Boa Noite!")
+    }**/
+
+
+    // -------- Exercícios de escrita de código --------//
+
+
+
+/*const verificaTurno = prompt("Qual Turno Você estuda? Use 'M' para Matutino, 'V' para Vespertino ou 'N' para Noturno")
+   
+switch (verificaTurno){
+
+    case "m":
+    console.log("Bom Dia!")
+    break
+
+    case "v":
+    console.log("Boa Tarde!")
+    break
+
+    case "n":
+    console.log("Boa Noite!")
+    break
+
+    default :
+    console.log("Não Temos Esse Horario Disponivel")
+    break
+
+}**/
+
+
+ // -------- Exercícios de escrita de código --------//
+
+
+ const generoFilme = prompt("qual generdo do filme vamos assitir")
+ const precoDoIngresso = Number(prompt("qual valor do ingresso"))
+
+ if(generoFilme === "fantasia", precoDoIngresso < 15){
+     console.log("Bom Filme!")
+ }
+
+ else{
+ console.log("escolha outo filme")
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
